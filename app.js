49c6242b59c3992c19108260715053719949a793
JustinBarnard1 <jbarnard0031@gmail.com>
2020-08-21 14:33:55 -0600

    Better Late Than Never

---
+++ app.js
@@ -0,0 +1,124 @@
+let weaponsElem = document.getElementById("weapon")
+let playerElem = document.getElementById("player")
+let helperElem = document.getElementById("helpers")
+
+let money = 0
+
+let player = [
+    {
+        name: prompt("Choose Your Character Name!"),
+        weapon: "Fist",
+        damage: 1,
+        cost: 0,
+    }
+]
+
+let weapons = [
+    {
+        name: "Wooden Pick",
+        damage: 5,
+        cost: 50,
+    },
+    {
+        name: "Broad Pick",
+        damage: 15,
+        cost: 250,
+    },
+    {
+        name: "Mithril Pick",
+        damage: 25,
+        cost: 500,
+    },
+    {
+        name: "Adamant Pick",
+        damage: 40,
+        cost: 1000,
+    },
+    {
+        name: "Rune Pick",
+        damage: 60,
+        cost: 10000,
+    },
+    {
+        name: "Magic Pick",
+        damage: 85,
+        cost: 100000,
+    },
+    {
+        name: "Ultimate Pick",
+        damage: 1000000,
+    }
+]
+let helpers = [
+    {
+        name: prompt("Name your helper!"),
+        damage: 50,
+        cost: 500,
+    }
+]
+
+function drawPlayer() {
+    let template = `${player.name} ${player.weapon.name}`
+    playerElem.innerHTML = template
+}
+
+function drawWeapons() {
+    let template = ""
+    weapons.forEach(weapon => {
+        template += getWeaponTemplate(weapon)
+    })
+    weaponsElem.innerHTML = template
+}
+
+function getWeaponTemplate(weapon) {
+    return `<button onclick="equipWeapon('${weapon.name}')">${weapon.name} ${weapon.damage} ${weapon.cost}</button>`
+}
+
+function drawHelper() {
+    let template = ""
+    helpers.forEach(helper => {
+        template += getHelperTemplate(helper)
+    })
+}
+
+let helperWhackIt = setInterval(helpers.damage, 2000)
+
+function helperWhacksIt() {
+    money += helpers.damage
+    updateBoard()
+}
+
+function getHelperTemplate(helper) {
+    return `<button onclick="addHelper('${helper.name}')">${helper.weapon} ${helper.cost}</button>`
+}
+
+function whackIt() {
+    money += weapon.damage
+}
+
+function equipWeapon(weaponName) {
+    let weapon = weapons.find(weapon => weapon.name == weaponName)
+    if (weapon.cost > money) {
+        return alert("You can't afford that.")
+    }
+    player.weapon = weapon
+    updateBoard()
+}
+
+function drawMoney() {
+
+}
+
+
+
+
+
+
+
+function updateBoard() {
+    drawWeapons()
+    drawPlayer()
+    drawHelper()
+}
+
+updateBoard()
